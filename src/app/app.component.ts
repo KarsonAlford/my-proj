12db746447c47e0a4fcf758e3e0166b1002bdc46
Karson Alford <karson.alford@gmail.com>
2017-04-18 09:01:15 -0500

    Add app.component.html.  Simple bindings to and from component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
+/*  template: `
+    <h1>Hello {{name}}</h1>
+    <div [style.color]="grassColor">
+      First Div Ever
+    </div>
+    <button (click)="changeGrassColor()">Change Grass Color</button>
+  `*/
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular';
+  grassColor = 'green';
+
+  changeGrassColor() {
+    if (this.grassColor == 'blue'){
+      this.grassColor = 'green';
+      return;
+    }
+    this.grassColor = 'blue';
+  }
+}
