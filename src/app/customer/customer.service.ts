4fccc601d235545bffd2899bb6e98ec622353798
KarsonAlford <karson.alford@fisglobal.com>
2017-04-19 08:07:12 -0500

    Module 5 with simple error handling

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -2,12 +2,20 @@ import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 
+import { Observable } from 'rxjs/Rx'
+
 @Injectable()
 export class CustomerService {
     constructor(private _http: Http) { }
 
     getCustomers() {
         return this._http.get('app/customer/customers.json')
-            .map((response: Response) => response.json());
+            .map((response: Response) => response.json())
+            .catch(this._handleError);
+    }
+
+    _handleError(err: any) {
+        console.log(err); // todo log this somewhere and format the message
+        return Observable.throw(err); // our opportunity to customize the error
     }
 }
