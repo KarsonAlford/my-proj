d8e083700149006b39d89f50a61bbe4afbfc794b
KarsonAlford <karson.alford@fisglobal.com>
2017-04-19 08:48:09 -0500

    Module 6 Barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CustomersComponent } from "./customers/customers.component"
-import { CustomerComponent } from "./customer/customer.component"
+import { CustomersComponent, CustomerComponent } from "./customer/index"
 
 @NgModule({
   imports: [
