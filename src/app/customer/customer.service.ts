f6f64cb96dbdc135b42076fe0de8f2c52c5a30b0
KarsonAlford <karson.alford@fisglobal.com>
2017-04-19 08:28:36 -0500

    Module 5 complete 4 ways to get data

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -8,7 +8,18 @@ import { Observable } from 'rxjs/Rx'
 export class CustomerService {
     constructor(private _http: Http) { }
 
+    // This is Ward's preference
     getCustomers() {
+        return this._http.get('app/customer/customers.json')
+            .map((response: Response) => response.json())
+            .toPromise()
+            .catch((err: any) => {
+                console.log(err); //todo again, customize me please
+                return Promise.reject(err);
+            });
+    }
+
+    getCustomers_RxObservable() {        
         return this._http.get('app/customer/customers.json')
             .map((response: Response) => response.json())
             .catch(this._handleError);
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
 
 import { CustomerService } from "../customer/customer.service"
 
@@ -11,15 +10,44 @@ import { CustomerService } from "../customer/customer.service"
 })
 
 export class CustomersComponent implements OnInit { 
-    customers: Observable<any[]>;
+    customers: any[];
 
     constructor(private _customerService: CustomerService) {}
 
     ngOnInit() {
-        this.customers = this._customerService.getCustomers()
-        .catch((err) => { 
-            console.log(err); // don't do this, show the user a nice message
-            return Observable.of([true]); // now we eat it, but only if the message has been communicated to the user
-        });
+        // Observable to array
+        this._customerService.getCustomers_RxObservable()
+        .subscribe(
+            // it worked
+            (customers) => this.customers = customers,
+            // failed
+            (err) => { console.log(err); }
+        );
+
+        // This is Ward's preference
+        // Straight up Promise to array
+        // this._customerService.getCustomers()
+        // .then((customers) => this.customers = customers)
+        // .catch((err) => { 
+        //     console.log(err); // don't do this, show the user a nice message
+        // });
+
+        // Promise version
+        // customers: Promise<any[]>;
+        // <li *ngFor="let c of customers | async">
+        // this.customers = this._customerService.getCustomers()
+        // .catch((err) => { 
+        //     console.log(err); // don't do this, show the user a nice message
+        // });
+
+        // Rx Observable version
+        // import { Observable } from 'rxjs/Rx';
+        // customers: Observable<any[]>;        
+        // <li *ngFor="let c of customers | async">
+        // this.customers = this._customerService.getCustomers_RxObservable()
+        // .catch((err) => { 
+        //     console.log(err); // don't do this, show the user a nice message
+        //     return Observable.of([true]); // now we eat it, but only if the message has been communicated to the user
+        // });
     }
 }
